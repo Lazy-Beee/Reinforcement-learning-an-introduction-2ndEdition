39e288e192089a691cf54b55b5c0f109081b3228
Lazy-Beee <junfan2017@gmail.com>
2021-06-28 17:13:09 -0400

    Completed chapter02

--- CH2/sample-bandit-algorithm.py
+++ CH2/sample-bandit-algorithm.py
@@ -16,30 +16,51 @@ class SampleMachine:
     Otherwise select the option with largest estimated reward.
     """
 
-    def __init__(self, eps=0.1, c=2, arm_num=10, params=(20, 50, 5),
-                 gen_fig=False, ucb_used=False):
+    def __init__(self, eps=0, alpha=0.1, c=2, arm_num=10, params=(10, 10, 1),
+                 gen_fig=True, method_used='', incremental_update=False, baseline=False):
         self.q = [0] * arm_num
         self.n = [0] * arm_num
         self.r = []
         self.options_count = arm_num
         self.eps = eps
+        self.alpha = alpha
         self.c = c
         self.gen_fig = gen_fig
-        self.ucb_used = ucb_used
+        # Method: ucb, gradient
+        self.method_used = method_used.lower()
+        self.incremental_update = incremental_update
+        self.baseline = baseline
         self.runs = 0
         self.avg_r = 0
         self.best_action_count = 0
-        self.record = {"q": [], "n": [], "bac": [], "avg_r": []}
+        self.action_prob = [0] * arm_num
+        self.record = {"q": [], "n": [], "bac": [], "avg_r": [], "r": []}
 
         self.bandit_machine = MultiArmBandit(arm_num=arm_num, params=params)
-        # self.best_action = self.bandit_machine.find_best_action()
+        self.check_inputs()
 
-    def act_eps_greedy(self):
+    def check_inputs(self):
+        if self.incremental_update and self.baseline:
+            print("Can not simultaneously apply: <incremental update> & <baseline>")
+            quit()
+
+        if self.method_used != "ucb" and self.method_used != "gradient" and self.method_used != '':
+            print(f"Can recognize method: {self.method_used}")
+            quit()
+
+        if self.method_used == "ucb" or self.method_used == "gradient":
+            self.eps = 0
+
+    def act(self):
         """Act based on possibilities, return the index of the chosen option"""
-        if np.random.random() >= self.eps:
-            return self._random_max_index(self.q)
+        if np.random.random() < self.eps:
+            return np.random.choice(np.arange(self.options_count))
+        elif self.method_used == 'ucb':
+            return self.act_ucb()
+        elif self.method_used == 'gradient':
+            return self.act_gradient()
         else:
-            return np.random.randint(0, self.options_count)
+            return self._random_max_index(self.q)
 
     def act_ucb(self):
         """Return the index of the largest ucb value"""
@@ -48,37 +69,49 @@ class SampleMachine:
             ucb_q[i] = self.q[i] + self.c * np.sqrt(np.log(self.runs) / (self.n[i] + 1e-5))
         return self._random_max_index(ucb_q)
 
+    def act_gradient(self):
+        exp_p = np.exp(self.q)
+        self.action_prob = exp_p / np.sum(exp_p)
+        return np.random.choice(np.arange(self.options_count), p=self.action_prob)
+
     def step(self, curr_arm):
         """Perform an act and update parameters"""
         if curr_arm == self.bandit_machine.find_best_action():
             self.best_action_count += 1
         self.r.append(self.bandit_machine.bandit(curr_arm))
-        self.avg_r = sum(self.r)/self.runs
+        self.avg_r += (self.r[-1] - self.avg_r) / self.runs
         self.n[curr_arm] += 1
-        self.q[curr_arm] += (self.r[-1] - self.q[curr_arm]) / self.n[curr_arm]
+        if self.incremental_update:
+            self.q[curr_arm] += self.alpha * (self.r[-1] - self.q[curr_arm])
+        elif self.baseline:
+            one_hot = np.zeros(self.options_count)
+            one_hot[curr_arm] = 1
+            baseline = self.avg_r
+            self.q += self.alpha * (self.r[-1] - baseline) * (one_hot - self.action_prob)
+        else:
+            self.q[curr_arm] += (self.r[-1] - self.q[curr_arm]) / self.n[curr_arm]
 
     def simulate(self, runs=1000):
         """Simulate, count and return how many times each option is chosen."""
-        for i in trange(runs):
+        for _ in trange(runs):
             self.runs += 1
-            if self.ucb_used:
-                curr_arm = self.act_ucb()
-            else:
-                curr_arm = self.act_eps_greedy()
+            curr_arm = self.act()
             self.step(curr_arm)
             self._record_data()
 
     def disp_result(self):
         """Output result to user"""
-        result_distribution = (np.array(self.n)/self.runs).tolist()
-        result_distribution = [float('%.2f' % elem) for elem in result_distribution]
-        q = [float('%.2f' % elem) for elem in self.q]
+        result_distribution = (np.array(self.n) / self.runs).tolist()
+        result_distribution = [float('%.1f' % (elem * 100)) for elem in result_distribution]
+        q = [float('%.1f' % elem) for elem in self.q]
+        bandit_machine_arms = [float('%.1f' % elem) for elem in self.bandit_machine.arms]
 
-        print(f"Iter: {self.runs}, \u03B5 = {self.eps}: {self.eps}")
-        print(f"Actions: {self.n}")
-        print(f"Distribution: {result_distribution}")
+        print(f"Method: {self.method_used}")
+        print(f"Iter: {self.runs}, \u03B5 = {self.eps}, \u03B1 = {self.alpha}")
+        print(f"Actions:          {self.n}")
+        print(f"Distribution(%):  {result_distribution}")
         print(f"Quality estimate: {q}")
-        print(f"Actually quality: {self.bandit_machine.arms}")
+        print(f"Actually quality: {bandit_machine_arms}")
 
         if self.gen_fig:
             steps = [i + 1 for i in range(self.runs)]
@@ -108,17 +141,8 @@ class SampleMachine:
         self.record["avg_r"].append(self.avg_r)
 
     def _random_max_index(self, values):
-        max_val = np.max(values)
-        i, ind = 0, []
-        for val in values:
-            if val == max_val:
-                ind.append(i)
-            i = i + 1
-        if len(ind) > 1:
-            ind = np.random.choice(ind)
-        else:
-            ind = ind[0]
-        return ind
+        q_best = np.max(values)
+        return np.random.choice(np.where(values == q_best)[0])
 
 
 class MultiArmBandit:
@@ -129,21 +153,21 @@ class MultiArmBandit:
     def __init__(self, arm_num, params):
         self.arm_num = arm_num
         self.arms = [0] * arm_num
-        # params = (lower boundary, upper boundary, standard division) of arm results
-        self.lower_bound = params[0]
-        self.upper_bound = params[1]
+        # params = (mean, std of arm value, standard division of arm output)
+        self.mean = params[0]
+        self.std = params[1]
         self.range = params[2]
         self._init_arm_params()
 
     def _init_arm_params(self):
         """Generate arm parameters"""
         for i in range(self.arm_num):
-            self.arms[i] = np.random.randint(self.lower_bound, self.upper_bound)
+            self.arms[i] = np.random.normal(self.mean, self.std)
 
     def bandit(self, arm_pressed):
         """Return result of arm_pressed"""
         arm_param = self.arms[arm_pressed]
-        return np.random.randint(arm_param - self.range, arm_param + self.range + 1)
+        return np.random.normal(arm_param, self.range)
 
     def find_best_action(self):
         """Return arm with highest reward"""
@@ -151,6 +175,7 @@ class MultiArmBandit:
 
 
 if __name__ == "__main__":
-    test_machine = SampleMachine(gen_fig=True, ucb_used=True)
-    test_machine.simulate(runs=10000)
+    test_machine = SampleMachine(gen_fig=True, method_used='gradient', incremental_update=False,
+                                 baseline=True)
+    test_machine.simulate(runs=1000)
     test_machine.disp_result()
